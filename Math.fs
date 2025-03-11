1230756b05289158d27924d18e55d58024b2a1b9
AAOctane <octanec8h18@kaist.ac.kr>
2025-03-11 09:55:24 +0900

    Update Math.fs

--- Math.fs
+++ Math.fs
@@ -1,9 +1,10 @@
-﻿module CS220.Quiz1.Math
+module CS220.Quiz1.Math
 
 open System
 
 /// Modify the function `collatz` in such a way that the function takes in a
 /// 32-bit integer, and returns the number of steps it takes to reach 1 in the
 /// Collatz sequence.
-let collatz n =
-  0
+let rec collatz n =
+  if n = 1 then 0 else
+    if n % 2 = 1 then collatz(3 * n + 1) + 1 else collatz(n / 2) + 1
