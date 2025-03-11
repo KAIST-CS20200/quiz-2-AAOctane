fa6a629fbeecf080c7c524e9ca893a8ba3632197
AAOctane <octanec8h18@kaist.ac.kr>
2025-03-11 10:03:08 +0900

    Update Math.fs

--- Math.fs
+++ Math.fs
@@ -5,6 +5,8 @@ open System
 /// Modify the function `collatz` in such a way that the function takes in a
 /// 32-bit integer, and returns the number of steps it takes to reach 1 in the
 /// Collatz sequence.
-let rec collatz n =
-  if n = 1 then 0 else
-    if n % 2 = 1 then collatz(3 * n + 1) + 1 else collatz(n / 2) + 1
+
+let rec collatz64 n: int64 = 
+  if n = 1L then 0L else
+    if n % 2L = 1L then collatz64(3L * n + 1L) + 1L else collatz64(n / 2L) + 1L
+let collatz n = int (collatz64 (int64 n))
